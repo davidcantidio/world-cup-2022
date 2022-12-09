82c219e4d3de9ce0a9703e98914ba3668983e71e
David Cantidio <dbcantidio@gmail.com>
2022-12-09 12:22:05 -0300

    matches and results showing up on main page

--- world_cup_2022/core/views.py
+++ world_cup_2022/core/views.py
@@ -11,28 +11,28 @@ def get_matches(request):
         url = 'http://api.football-data.org/v4/competitions/2000/matches'
         response = requests.get(url, headers=headers)
         data = response.json()
-        pprint(data['matches'][0])
         for d in data['matches']:
-            matches = Match(
-                startDate = d['utcDate'],
-                winner = d['score']['winner'],
-                stage = d['stage'],
-                status = d['status'],
-                awayTeam_name = d['awayTeam']['name'],
-                awayTeam_shortName = d['awayTeam']['shortName'],
-                awayTeam_tla = d['awayTeam']['tla'],
-                awayTeam_crest_url = d['awayTeam']['crest'],
-                homeTeam_name = d['homeTeam']['name'],
-                homeTeam_shortName = d['homeTeam']['shortName'],
-                homeTeam_tla = d['homeTeam']['tla'],
-                homeTeam_crest_url = d['homeTeam']['crest'],
-                comptetition_name = d['competition']['name'],
-                competition_emblem = d['competition']['emblem'],
-                group = d['group'],
-                match_duration = d['score']['duration'],
-                score_fullTime_awayTeam = d['score']['fullTime']['away'],
-                score_fullTime_homeTeam = d['score']['fullTime']['home'],
-            )
-            matches.save()
-            all_matches = Match.objects.all().order_by('-id')
+            if d['awayTeam']['name']:
+                matches = Match(
+                    startDate = d['utcDate'],
+                    winner = d['score']['winner'],
+                    stage = d['stage'],
+                    status = d['status'],
+                    awayTeam_name = d['awayTeam']['name'],
+                    awayTeam_shortName = d['awayTeam']['shortName'],
+                    awayTeam_tla = d['awayTeam']['tla'],
+                    awayTeam_crest_url = d['awayTeam']['crest'],
+                    homeTeam_name = d['homeTeam']['name'],
+                    homeTeam_shortName = d['homeTeam']['shortName'],
+                    homeTeam_tla = d['homeTeam']['tla'],
+                    homeTeam_crest_url = d['homeTeam']['crest'],
+                    comptetition_name = d['competition']['name'],
+                    competition_emblem = d['competition']['emblem'],
+                    group = d['group'],
+                    match_duration = d['score']['duration'],
+                    score_fullTime_awayTeam = d['score']['fullTime']['away'],
+                    score_fullTime_homeTeam = d['score']['fullTime']['home'],
+                )
+                matches.save()
+                all_matches = Match.objects.all().order_by('-id')
     return render(request, 'base.html', {'all_matches':all_matches} )
