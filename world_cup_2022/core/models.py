f92c48814181a2f549b6b42c07f8e1309f341242
David Cantidio <dbcantidio@gmail.com>
2022-12-10 17:10:43 -0300

    Revert "displaying mtches info and countries flags"
    This reverts commit beea7dea1d54254622b09b1fb8ea1818b91a2c3a.

--- world_cup_2022/core/models.py
+++ world_cup_2022/core/models.py
@@ -1,19 +1,15 @@
 from django.db import models
-from django.utils import timezone
 
 # Create your models here.
 class Match(models.Model):
-    utcDate = models.CharField(max_length=50, null=True)
     startDate = models.CharField(max_length=50)
     winner = models.CharField(max_length=90, null=True)
     stage = models.CharField(max_length=40, null=True)
     status = models.CharField(max_length=20, null=True)
-    awayTeam_score_color = models.CharField(max_length=20, null=True)
     awayTeam_name = models.CharField(max_length=40, null=True)
     awayTeam_shortName = models.CharField(max_length=40, null=True)
     awayTeam_tla = models.CharField(max_length=4, null=True)
     awayTeam_crest_url = models.CharField(max_length=200, null=True)
-    homeTeam_score_color = models.CharField(max_length=20, null=True)
     homeTeam_name = models.CharField(max_length=40, null=True)
     homeTeam_shortName = models.CharField(max_length=40, null=True)
     homeTeam_tla = models.CharField(max_length=4, null=True)
@@ -25,13 +21,6 @@ class Match(models.Model):
     score_fullTime_awayTeam = models.IntegerField(null=True)
     score_fullTime_homeTeam = models.IntegerField(null=True)
 
-    class Meta:
-        ordering = ('-startDate',)
-
-    def __str__(self):
-        label = f'{self.homeTeam_tla} vs {self.awayTeam_tla}'
-        return label
-    
-    @property
-    def is_in_past(self):
-        return self.startDate < timezone.now()
+def __str__(self):
+    label = f'{self.homeTeam_tla} vs {self.awayTeam_tla}'
+    return label
